78fa893e3730bb5aa45953381dbc55365df2f3c1
Igor Solowyov <solovyow_iv@nfksber.ru>
2018-04-27 14:10:32 +0300

    Сделано добавление в таблицу

---
+++ web/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.list-group-item').on('click', function(){
+        var text_el = $(this).text();
+        var text_ed = $(this).attr('data-ed');
+        var text_id = $(this).attr('id');
+        console.log('это '+text_el);
+        $('#blok_1').append('<tr><td>'+text_id+'</td><td>'+text_el+'</td><td>'+text_ed+'</td><td><input value="1" /></td></tr>');
+    });
+});
