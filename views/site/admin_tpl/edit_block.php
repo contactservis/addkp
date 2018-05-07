5fbbbd03bff8913b29e368005472452e34c5a2db
Igor Solowyov <solovyow_iv@nfksber.ru>
2018-05-07 17:44:32 +0300

    добавление данных из бд в админ панель

--- views/site/admin_tpl/edit_block.php
+++ views/site/admin_tpl/edit_block.php
@@ -1,5 +1,5 @@
 <?php
- print_r($nameBlocks);
+
 ?>
 <div class="row">
     <h3>Список блоков для КП</h3>
@@ -14,23 +14,24 @@
                 <?php foreach($Items as $tblItems){ ?>
                 <div class="panel-group" id="accordion">
                     <div class="panel panel-default">
-                        <div class="panel-heading" id="7">
+                        <div class="panel-heading" id="<?=$tblItems['id']?>">
                             <h4 class="panel-title">
-                                <a data-toggle="collapse" data-parent="#accordion" href="#collapse7" aria-expanded="true"><?=$tblItems['name']?></a>
+                                <a data-toggle="collapse" data-parent="#accordion" href="#collapse<?=$tblItems['id']?>" aria-expanded="true"><?=$tblItems['name']?></a>
                             </h4>
                         </div>
-                        <div id="collapse7" class="panel-collapse collapse in" aria-expanded="true" style="">
-                            <div class="panel-body" style="padding:0;">
-                                <ul class="list-group" style="margin-bottom:0;">
-                                    <li class="list-group-item" id="7" data-ed="">
-                                        <p>Каркас</p>
-                                        <div contenteditable="true">
-                                            Каркас металлический стальной высокопрочный, прямоугольный из сложногнутых профилей заводского исполнения. Является основным несущим элементом. Нижний и верхний периметр из швеллера 100*50*3, потолочная лага и стойки усиления – швеллер 50*40*3, угловые стройки – угол г/к 63*63*5, окрашен грунт + эмаль в 2 слоя ХВ-0278. Соединения рам со стойками сварные. Жесткость каркаса в целом обеспечивается системой распорок, вертикальных и горизонтальных связей. Блок – контейнер имеет устройства (ушки), обеспечивающие безопасную и надежную перевозку и крепление на транспортных средствах.
-                                        </div>
-                                        <button type="button" id="add_item_obdj" class="btn btn-success" data-toggle="modal" data-target="#w0">Сохранить изменения</button>
-                                    </li>
-                                    <li class="list-group-item" id="9" data-ed="Каркас металлический стальной высокопрочный, прямоугольный из сложногнутых профилей заводского исполнения. Является основным несущим элементом. Нижний и верхний периметр из швеллера 100*50*3, потолочная лага и стойки усиления – швеллер 50*40*3, угловые стройки – угол г/к 63*63*5, окрашен грунт + эмаль в 2 слоя ХВ-0278. Соединения рам со стойками сварные. Жесткость каркаса в целом обеспечивается системой распорок, вертикальных и горизонтальных связей. Блок – контейнер имеет устройства (ушки), обеспечивающие безопасную и надежную перевозку и крепление на транспортных средствах. ">Каркас металлический стальной высокопрочный</li>
-                                </ul>
+                        <div id="collapse<?=$tblItems['id']?>" class="panel-collapse collapse" aria-expanded="true" style="">
+                            <div class="panel-body" style="padding:0;">                                
+                                <?php foreach($tblItems['item'] as $listItem) { ?>
+                                    <ul class="list-group" style="margin-bottom:0;">
+                                        <li class="list-group-item" id="7" data-ed="">
+                                            <h4 contenteditable="true"><?php echo $listItem['name'] ;?></h4>
+                                            <div contenteditable="true">
+                                                <?=$listItem['desc']?>
+                                            </div>
+                                            <button type="button" id="add_item_obdj" class="btn btn-success" data-toggle="modal" data-target="#w0" style="margin-top: 15px;">Сохранить изменения</button>
+                                        </li>                                    
+                                    </ul>
+                                <?php } ?>
                             </div>
                         </div>
                     </div>            
